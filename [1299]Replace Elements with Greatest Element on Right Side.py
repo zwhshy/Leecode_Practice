5e4df2e79904a8d8ac4d9e1d708606ce6a9567bc
zwhsh <zwhshy@sina.com>
2024-04-30 01:39:33 -0400

    Add all files

---
+++ [1299]Replace Elements with Greatest Element on Right Side.py
@@ -0,0 +1,52 @@
+# Given an array arr, replace every element in that array with the greatest 
+# element among the elements to its right, and replace the last element with -1. 
+# 
+#  After doing so, return the array. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: arr = [17,18,5,4,6,1]
+# Output: [18,6,6,6,1,-1]
+# Explanation: 
+# - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+# - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+# - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+# - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+# - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+# - index 5 --> there are no elements to the right of index 5, so we put -1.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: arr = [400]
+# Output: [-1]
+# Explanation: There are no elements to the right of index 0.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= arr.length <= 10⁴ 
+#  1 <= arr[i] <= 10⁵ 
+#  
+# 
+#  Related Topics Array 👍 2540 👎 239
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        max_num = - 1
+        for item in range(len(arr) - 1, -1, -1):
+            temp = max_num
+            arr[item], max_num = max_num, max(max_num, arr[item])
+        return arr
+# leetcode submit region end(Prohibit modification and deletion)
